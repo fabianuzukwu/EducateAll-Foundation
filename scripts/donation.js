0335aefaf5950c8dd6ac004381bcaa96e67abcdc
Nedu Robert <nedumrobert@gmail.com>
2019-09-01 17:24:57 +0100

    feat: updated flutterwave integration

--- scripts/donation.js
+++ scripts/donation.js
@@ -1,20 +1,29 @@
 const API_publicKey = "FLWPUBK-ee4f50e2181a7717274f1ef058927eae-X";
 
 function payWithRave() {
+    const email = document.getElementById('email').value;
+    const firstname = document.getElementById('firstname').value;
+    const lastname = document.getElementById('lastname').value;
+    const phone = document.getElementById('phone').value;
+    const amounts = document.getElementsByName('amount');
+    let amount_value;
+    for(var i = 0; i < amounts.length; i++){
+        if(amounts[i].checked){
+            amount_value = amounts[i].value;
+        }
+    }
+
+    const amount = amount_value || Number(document.getElementById('amount').value);
     var x = getpaidSetup({
         PBFPubKey: API_publicKey,
-        customer_email: "test@test.com",
-        customer_firstname: "Test",
-        customer_lastname: "Testing",
-        amount: 2000,
-        customer_phone: "234099940409",
+        customer_email: email,
+        customer_firstname: firstname,
+        customer_lastname: lastname,
+        amount: amount,
+        customer_phone: phone,
         currency: "NGN",
         country: "NG",
         txref: "rave-123456",
-        meta: [{
-            metaname: "flightID",
-            metavalue: "AP1234"
-        }],
         onclose: function() {},
         callback: function(response) {
             var txref = response.tx.txRef; // collect txRef returned and pass to a server page to complete status check.
