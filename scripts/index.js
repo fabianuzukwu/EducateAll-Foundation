66f0553ab7ad9e31974411c34b9b7c5d70a6199e
Johnson Ubaezuonu <jaycodist@gmail.com>
2019-09-03 23:55:58 +0100

    Fixed layout issues with article links @Johnson

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+document.getElementById("first-article").addEventListener("click", () => 
+	location.href = "articles_essence.html");
+
+document.getElementById("second-article").addEventListener("click", () => 
+	location.href = "articles_challenge.html");
+
+document.getElementById("third-article").addEventListener("click", () => 
+	location.href = "articles_giving.html");
